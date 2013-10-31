8286c51911b6722d1fe5ac4d5a71d46f673407ce
Wencan Luo <wencanluo.cn@gmail.com>
2013-10-30 21:51:27 -0400

    update scripts

--- src/SigDial2014/scripts/run.bat
+++ src/SigDial2014/scripts/run.bat
@@ -3,6 +3,13 @@ set ontology=config/ontology_dstc2.json
 set outdir=res/
 set CRFDir=D:/NLP/CRF++-0.58/
 
+rem goto after_summary
+set m=summary
+for %%t in (dstc2_train dstc2_dev) do (
+	python getSummary.py --dataset=%%t --dataroot=%root% --logfile=%outdir%%m%_%%t.txt
+)
+:after_summary
+
 goto after_HWUbaseline
 set m=HWUbaseline
 for %%t in (dstc2_train dstc2_dev) do (
@@ -21,7 +28,7 @@ for %%t in (dstc2_train dstc2_dev) do (
 )
 :after_baseline
 
-rem goto after_baseline_focus
+goto after_baseline_focus
 set m=baseline_focus
 for %%t in (dstc2_train dstc2_dev) do (
 	python baseline.py --dataset=%%t --dataroot=%root% --trackfile=%outdir%%m%_%%t_track.json --focus=True
